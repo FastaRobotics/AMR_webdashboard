2749b7cc7428e16615d936a16d88c775e9326e18
Aida Mobli <aidaw.mobli@gmail.com>
2025-08-16 10:17:42 +0000

    make names consistant

--- backend/amr_robot.py
+++ backend/amr_robot.py
@@ -1,9 +1,10 @@
-from ros_bridge.AMR.publisher_available import TOPIC_MESSAGE_TYPES
+from ros_bridge.AMR.publisher_available import PUBLISHABLE_TOPIC_MESSAGE_TYPES
 from ros_bridge.AMR.subscriber_available import SUBSCRIBABLE_TOPIC_MESSAGE_TYPES
 from robot import Robot
 
 class AMR (Robot):
     def __init__(self, robot_id: str, port: int):
         super().__init__(robot_id, 
-                         port,TOPIC_MESSAGE_TYPES=TOPIC_MESSAGE_TYPES, 
+                         port,
+                         PUBLISHABLE_TOPIC_MESSAGE_TYPES=PUBLISHABLE_TOPIC_MESSAGE_TYPES, 
                          SUBSCRIBABLE_TOPIC_MESSAGE_TYPES=SUBSCRIBABLE_TOPIC_MESSAGE_TYPES)
--- backend/go2_robot.py
+++ backend/go2_robot.py
@@ -1,4 +1,4 @@
-from ros_bridge.go2.publisher_available import TOPIC_MESSAGE_TYPES
+from ros_bridge.go2.publisher_available import PUBLISHABLE_TOPIC_MESSAGE_TYPES
 from ros_bridge.go2.subscriber_available import SUBSCRIBABLE_TOPIC_MESSAGE_TYPES
 from robot import Robot
 
@@ -6,5 +6,6 @@ from robot import Robot
 class Go2 (Robot):
     def __init__(self, robot_id: str, port: int):
         super().__init__(robot_id, port,
-                         TOPIC_MESSAGE_TYPES=TOPIC_MESSAGE_TYPES, 
-                         SUBSCRIBABLE_TOPIC_MESSAGE_TYPES=SUBSCRIBABLE_TOPIC_MESSAGE_TYPES)
+                         PUBLISHABLE_TOPIC_MESSAGE_TYPES=PUBLISHABLE_TOPIC_MESSAGE_TYPES, 
+                         SUBSCRIBABLE_TOPIC_MESSAGE_TYPES=SUBSCRIBABLE_TOPIC_MESSAGE_TYPES
+                         )
